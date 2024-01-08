e5e6d79f8e918aeaace14d6e7928d213fcc1f02c
NDoolan360 <n.doolan360@gmail.com>
2024-01-08 13:10:24 +1100

    Add local server for dev

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/ndoolan360/go-htmx-site/api"
+)
+
+var apiHandlers = map[string]func(http.ResponseWriter, *http.Request){
+	"/api/copyright": api.GetCopyright,
+	"/api/projects":  api.GetProjects,
+}
+
+func main() {
+	for endpoint, handler := range apiHandlers {
+		http.HandleFunc(endpoint, handler)
+	}
+	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("./public"))))
+
+	port := ":3000"
+	fmt.Printf("Server listening on port %s\n", port)
+	http.ListenAndServe(port, nil)
+}
