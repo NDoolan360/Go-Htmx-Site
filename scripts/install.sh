63b63dd06273b515962d6bcd841712f1995825cd
Nathan Doolan <nathan.doolan@kaluza.com>
2024-02-01 17:45:17 +1100

    Multiplatform install support

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
 
-# Tailwind CSS
-curl -sLO https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-linux-x64
-chmod +x tailwindcss-linux-x64
-mv tailwindcss-linux-x64 tailwindcss
+# Default values
+PLATFORM="x64"
+OS="linux"
+
+# Parse optional command-line arguments
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    -p|--platform)
+      PLATFORM="$2"
+      shift
+      ;;
+    -o|--os)
+      OS="$2"
+      shift
+      ;;
+    *)
+      echo "Unknown option: $1"
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+# Build Tailwind CSS download URL
+URL="https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-${OS}-${PLATFORM}"
+
+# Download Tailwind CSS
+curl -sLO "$URL"
+chmod +x "tailwindcss-${OS}-${PLATFORM}"
+mv "tailwindcss-${OS}-${PLATFORM}" tailwindcss
