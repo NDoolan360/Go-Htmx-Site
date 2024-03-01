42d4e20cc7602b76b6ec44faa631aee997a9b8bd
Nathan Doolan <nathan.doolan@kaluza.com>
2024-03-01 19:58:46 +1100

    overhaul project logic
    - Move cults3d to graphql api
    - Move bgg to geeklist and use xml api
    - Refine structure and use more Unmarshalling

--- api/projects.go
+++ api/projects.go
@@ -3,15 +3,16 @@ package api
 import (
 	"encoding/json"
 	"encoding/xml"
+	"errors"
 	"fmt"
 	"html/template"
 	"io"
 	"net/http"
-	"regexp"
+	"os"
 	"strings"
+	"time"
 
-	githubLanguage "github.com/NDoolan360/github-langs-go"
-	"golang.org/x/net/html"
+	githublangsgo "github.com/NDoolan360/github-langs-go"
 )
 
 // GetProjects handles the request for fetching and rendering project data.
@@ -22,262 +23,210 @@ func GetProjects(w http.ResponseWriter, r *http.Request) {
 			GetApiAsset("template/projects.gohtml"),
 		))
 		err := projectTemplate.Execute(w, ProjectsTemplate{Projects: projects})
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-	} else {
-		errorMessages := ""
-		for _, err := range errs {
-			errorMessages += err.Error() + "\n"
-		}
-		http.Error(w, errorMessages, http.StatusInternalServerError)
+		errs = errors.Join(errs, err)
+	}
+	if errs != nil {
+		http.Error(w, errs.Error(), http.StatusInternalServerError)
 	}
 }
 
-// HostMap contains information about different project hosts for fetching and parsing content.
-var HostMap = map[string]Host{
-	"github": {
-		Name: "Github",
-		Path: "https://api.github.com/users/NDoolan360/repos?sort=stars",
-		Type: "json",
-	},
-	"bgg": {
-		Name:   "Board Game Geek",
-		Path:   "https://boardgamegeek.com/geeksearch.php?action=search&advsearch=1&objecttype=boardgame&include%5Bdesignerid%5D=133893",
-		Type:   "html",
-		Parser: BGGNode,
-	},
-	"cults3d": {
-		Name:   "Cults3D",
-		Path:   "https://cults3d.com/en/users/ND360/3d-models",
-		Type:   "html",
-		Parser: Cults3DNode,
-	},
-}
-
-func FetchProjects(hostNames []string) ([]Project, []error) {
-	projects := []Project{}
-	errs := []error{}
+func FetchProjects(hostNames []string) (projects []Project, errs error) {
 	for _, hostName := range hostNames {
-		if host, ok := HostMap[hostName]; !ok {
-			errs = append(errs, fmt.Errorf("URL not found for host: %s", hostName))
-
-		} else if content, fetchErr := Fetch(host.Path); fetchErr != nil {
-			errs = append(errs, fmt.Errorf("error fetching content from host %s: %s", hostName, fetchErr.Error()))
-
-		} else if hostProjects, ParseErr := Parse(host, content); ParseErr != nil {
-			errs = append(errs, fmt.Errorf("error parsing content from host %s: %s", hostName, ParseErr.Error()))
-
-		} else {
-			for _, project := range hostProjects {
-				// Skip unimportant Github Repos
-				if hostName == "github" && (project.Fork || len(project.Topics) == 0) {
-					continue
-				}
-				project.Host = host.Name
-				project.LogoSVG = GetSVGLogo(hostName)
-				if project.Language != "" {
-					if lang, err := githubLanguage.GetLanguage(project.Language); err == nil {
-						project.LanguageColour = template.CSS(lang.Color)
-					}
-				}
-				if hostName == "bgg" {
-					if err := UpgradeBGG(project); err != nil {
-						continue
-					}
-				}
-				projects = append(projects, *project)
+		host, ok := hostMap[hostName]
+		if ok {
+			data, err := Fetch(&host.Request)
+			if err != nil {
+				errs = errors.Join(errs, err)
 			}
+			newProjects, err := host.Parser(data)
+			if err != nil {
+				errs = errors.Join(errs, err)
+			}
+			projects = append(projects, newProjects...)
+		} else {
+			errs = errors.Join(errs, fmt.Errorf("URL not found for host: %s", hostName))
 		}
 	}
-
 	return projects, errs
 }
 
 // Fetch fetches content from a given URL using HTTP GET.
-var Fetch = func(url string) (string, error) {
-	if resp, err := http.Get(url); err != nil {
-		return "", err
-	} else {
-		defer resp.Body.Close()
-
-		if resp.StatusCode != http.StatusOK {
-			return "", fmt.Errorf("HTTP request failed with status code: %d", resp.StatusCode)
-		} else if body, err := io.ReadAll(resp.Body); err != nil {
-			return "", err
-		} else {
-			return string(body), nil
-		}
-	}
-}
-
-// Parse parses content based on host and content type, returning a list of projects.
-func Parse(host Host, content string) ([]*Project, error) {
-	var projects []*Project
-	var err error
-	switch host.Type {
-	case "json":
-		err = json.Unmarshal([]byte(content), &projects)
-	case "html":
-		doc, parseErr := html.Parse(strings.NewReader(content))
-		if parseErr != nil {
-			err = fmt.Errorf("error parsing HTML: %s", parseErr)
-		}
-		if host.Parser != nil {
-			projects = ParseHTMLDoc(doc, host.Parser)
-		} else {
-			err = fmt.Errorf("no parser provided for host: %s", host.Name)
-		}
-	default:
-		err = fmt.Errorf("unsupported content type: %s", host.Type)
-	}
-
+var Fetch = func(request *Request) ([]byte, error) {
+	body := strings.NewReader(request.Body)
+	outgoingRequest, err := http.NewRequest(request.Method, request.Path, body)
 	if err != nil {
 		return nil, err
 	}
-	return projects, nil
-}
-
-// Functions for HTML parsing and helpers.
-
-type NodePredicate[T any] func(*html.Node) (*T, bool)
-
-func ParseHTMLDoc[T any](node *html.Node, check NodePredicate[T]) (collection []*T) {
-	if object, ok := check(node); ok {
-		collection = append(collection, object)
+	if len(request.ContentType) > 0 {
+		outgoingRequest.Header.Add("Content-Type", "application/json")
+	}
+	if len(request.Username) > 0 && len(request.Password) > 0 {
+		outgoingRequest.SetBasicAuth(request.Username, request.Password)
 	}
 
-	for next := node.FirstChild; next != nil; next = next.NextSibling {
-		collection = append(collection, ParseHTMLDoc(next, check)...)
+	client := &http.Client{Timeout: time.Second * 10}
+	response, err := client.Do(outgoingRequest)
+	if err != nil {
+		return nil, err
 	}
+	defer response.Body.Close()
 
-	return collection
+	if data, err := io.ReadAll(response.Body); err != nil {
+		return nil, err
+	} else {
+		return data, nil
+	}
 }
 
-// BGGNode is a parser function for Board Game Geek (BGG) HTML content.
-func BGGNode(node *html.Node) (*Project, bool) {
-	if node.Data == "tr" && WithTagEqual("id", "row_")(node) {
-		project := Project{}
-		if title := FirstInChildren(node, WithTagEqual("class", "primary")); title != nil {
-			project.Title = GetTextContent(title)
-		}
-		if description := FirstInChildren(node, WithTagEqual("class", "smallefont")); description != nil {
-			project.Description = GetTextContent(description)
-		}
-		if thumbnail := FirstInChildren(node, WithTagEqual("class", "collection_thumbnail")); thumbnail != nil {
-			if a := FirstInChildren(thumbnail, WithTag("a")); a != nil {
-				project.HtmlUrl = fmt.Sprintf("https://boardgamegeek.com%s", GetAttribute(a, "href"))
-				if img := FirstInChildren(a, WithTag("img")); img != nil {
-					project.ImageSrc = template.HTMLAttr(fmt.Sprintf("src=\"%s\"", GetAttribute(img, "src")))
-					project.ImageAlt = template.HTMLAttr(fmt.Sprintf("alt=\"%s\"", GetAttribute(img, "alt")))
+var hostMap = map[string]Host{
+	"github": {
+		Request: Request{
+			Method: "GET",
+			Path:   "https://api.github.com/users/NDoolan360/repos?sort=stars",
+		},
+		Parser: func(data []byte) (projects []Project, err error) {
+			var githubProjects []struct {
+				Title       string   `json:"name"`
+				Description string   `json:"description"`
+				Url         string   `json:"html_url"`
+				Language    string   `json:"language"`
+				Topics      []string `json:"topics"`
+				Fork        bool     `json:"fork"`
+			}
+
+			if unmarshalErr := json.Unmarshal(data, &githubProjects); unmarshalErr != nil {
+				return nil, errors.Join(errors.New("error parsing GitHub projects"), unmarshalErr)
+			}
+
+			for _, project := range githubProjects {
+				if project.Fork || len(project.Topics) == 0 {
+					continue
 				}
-			}
-		}
-		return &project, true
-	}
 
-	return nil, false
-}
+				var colour template.CSS
+				if lang, languageErr := githublangsgo.GetLanguage(project.Language); languageErr != nil {
+					err = errors.Join(err, fmt.Errorf("error parsing language (%s)", project.Language), languageErr)
+				} else {
+					colour = template.CSS(lang.Color)
+				}
 
-// Cults3DNode is a parser function for Cults3D HTML content.
-func Cults3DNode(node *html.Node) (*Project, bool) {
-	if node.Data == "article" && WithTagEqual("class", "crea")(node) {
-		project := Project{}
-		if h3 := FirstInChildren(node, WithTag("h3")); h3 != nil {
-			project.Title = GetTextContent(h3)
-		}
-		if a := FirstInChildren(node, WithTag("a")); a != nil {
-			project.HtmlUrl = fmt.Sprintf("https://cults3d.com%s", GetAttribute(a, "href"))
-		}
-		if img := FirstInChildren(node, WithTag("img")); img != nil {
-			dataSrc := GetAttribute(img, "data-src")
-
-			// extract full size file rather than thumbnail image if possible
-			regex := regexp.MustCompile(`https://files\.cults3d\.com[^'"]+`)
-			if match := regex.FindString(dataSrc); match != "" {
-				dataSrc = match
+				projects = append(projects, Project{
+					Host:  "Github",
+					Title: project.Title,
+					Url:   project.Url,
+					Language: Language{
+						Name:   project.Language,
+						Colour: colour,
+					},
+					Logo:        GetSVGLogo("github"),
+					Description: template.HTML(project.Description),
+					Topics:      project.Topics,
+				})
 			}
 
-			project.ImageSrc = template.HTMLAttr(fmt.Sprintf("src=\"%s\"", dataSrc))
-			project.ImageAlt = template.HTMLAttr(fmt.Sprintf("alt=\"%s\"", GetAttribute(img, "alt")))
-		}
-		return &project, true
-	}
+			return projects, err
+		},
+	},
+	"cults3d": {
+		Request: Request{
+			Method:   "POST",
+			Path:     "https://cults3d.com/graphql",
+			Username: os.Getenv("CULTS3D_USERNAME"),
+			Password: os.Getenv("CULTS3D_API_KEY"),
+			Body: `{
+				"query": "{
+					creationsBatch(limit: 5, creatorNick: "ND360", sort: BY_DOWNLOADS) {
+						results {
+							name(locale: EN)
+							url(locale: EN)
+							illustrationImageUrl
+							tags
+						}
+					}
+				}"
+			}`,
+		},
+		Parser: func(data []byte) (projects []Project, err error) {
+			var cults3dProjects struct {
+				Data struct {
+					CreationsBatch struct {
+						Results []struct {
+							Title       string   `json:"name"`
+							Description string   `json:"description"`
+							Url         string   `json:"url"`
+							ImageSrc    string   `json:"illustrationImageUrl"`
+							Topics      []string `json:"tags"`
+						} `json:"results"`
+					} `json:"creationsBatch"`
+				} `json:"data"`
+			}
+			if unmarshalErr := json.Unmarshal(data, &cults3dProjects); unmarshalErr != nil {
+				return nil, errors.Join(errors.New("error parsing Cults3D projects"), unmarshalErr)
+			}
 
-	return nil, false
-}
+			for _, project := range cults3dProjects.Data.CreationsBatch.Results {
+				projects = append(projects, Project{
+					Host:        "Cults3D",
+					Title:       project.Title,
+					Description: template.HTML(project.Description),
+					Url:         project.Url,
+					Image: Image{
+						Src: template.HTMLAttr(fmt.Sprintf("src=\"%s\"", project.ImageSrc)),
+						Alt: template.HTMLAttr(fmt.Sprintf("alt=\"3D Model: %s\"", project.Title)),
+					},
+					Logo:   GetSVGLogo("cults3d"),
+					Topics: project.Topics,
+				})
+			}
 
-// HTML parsing helpers.
+			return projects, err
+		},
+	},
+	"bgg": {
+		Request: Request{
+			Method: "GET",
+			Path:   "https://boardgamegeek.com/xmlapi/geeklist/332832",
+		},
+		Parser: func(data []byte) (projects []Project, err error) {
+			var projectItems []struct {
+				Item struct {
+					Id string `xml:"objectid,attr"`
+				} `xml:"item"`
+			}
+			if unmarshalErr := xml.Unmarshal(data, &projectItems); unmarshalErr != nil {
+				return nil, errors.Join(errors.New("error parsing BGG projects"), unmarshalErr)
+			}
 
-func GetAttribute(n *html.Node, attrName string) string {
-	for _, attr := range n.Attr {
-		if attr.Key == attrName {
-			return attr.Val
-		}
-	}
-	return ""
-}
+			for _, item := range projectItems {
+				projectData, _ := Fetch(&Request{
+					Method: "GET",
+					Path:   fmt.Sprintf("https://boardgamegeek.com/xmlapi/boardgame/%s", item.Item.Id),
+				})
+				var bggProject struct {
+					Title    string   `xml:"boardgame>name"`
+					ImageSrc string   `xml:"boardgame>image"`
+					Tags     []string `xml:"boardgame>boardgamemechanic"`
+				}
 
-func GetTextContent(n *html.Node) string {
-	return strings.TrimSpace(n.FirstChild.Data)
-}
+				if unmarshalErr := xml.Unmarshal(projectData, &bggProject); unmarshalErr != nil {
+					err = errors.Join(err, fmt.Errorf("error parsing BGG project (%s)", item.Item.Id), unmarshalErr)
+					continue
+				}
 
-type MatchPredicate func(*html.Node) bool
+				projects = append(projects, Project{
+					Host:  "Board Game Geek",
+					Title: bggProject.Title,
+					Url:   fmt.Sprintf("https://boardgamegeek.com/boardgame/%s", item.Item.Id),
+					Image: Image{
+						Src: template.HTMLAttr(fmt.Sprintf("src=\"%s\"", bggProject.ImageSrc)),
+						Alt: template.HTMLAttr(fmt.Sprintf("alt=\"Board Game: %s\"", bggProject.Title)),
+					},
+					Logo:   GetSVGLogo("bgg"),
+					Topics: bggProject.Tags,
+				})
+			}
 
-func FirstInChildren(node *html.Node, match MatchPredicate) *html.Node {
-	if node == nil {
-		return nil
-	}
-	if match(node) {
-		return node
-	}
-
-	for c := node.FirstChild; c != nil; c = c.NextSibling {
-		if found := FirstInChildren(c, match); found != nil {
-			return found
-		}
-	}
-
-	return nil
-}
-
-func WithTag(tag string) MatchPredicate {
-	return func(node *html.Node) bool {
-		return node.Data == tag
-	}
-}
-
-func WithTagEqual(tag string, value string) MatchPredicate {
-	return func(node *html.Node) bool {
-		return strings.Contains(GetAttribute(node, tag), value)
-	}
-}
-
-func UpgradeBGG(project *Project) error {
-	re := regexp.MustCompile(`/boardgame/(\d+)`)
-	matches := re.FindStringSubmatch(project.HtmlUrl)
-	if len(matches) < 2 {
-		return fmt.Errorf("boardgame ID not found in the URL")
-	}
-	boardgameID := matches[1]
-
-	bggXML, fetchErr := Fetch(fmt.Sprintf("https://api.geekdo.com/xmlapi/boardgame/%s", boardgameID))
-	if fetchErr != nil {
-		return fetchErr
-	}
-
-	var boardGames BoardGames
-	parseErr := xml.Unmarshal([]byte(bggXML), &boardGames)
-	if parseErr != nil {
-		return fmt.Errorf("error un-marshaling XML: %s", parseErr)
-	}
-
-	if boardGames.BoardGame.ImageURL != "" {
-		project.ImageSrc = template.HTMLAttr(fmt.Sprintf(`src="%s"`, boardGames.BoardGame.ImageURL))
-	}
-	if len(boardGames.BoardGame.Mechanics) > 0 {
-		project.Topics = boardGames.BoardGame.Mechanics
-	}
-
-	return nil
+			return projects, err
+		},
+	},
 }
--- api/projects_test.go
+++ api/projects_test.go
@@ -19,77 +19,60 @@ func TestFetchProjects(t *testing.T) {
 		{"Cults3D Projects Test", []string{"cults3d"}, Cults3DExpected},
 		{"All Projects Test", []string{"github", "bgg", "cults3d"}, append(append(GithubExpected, BggExpected...), Cults3DExpected...)},
 	}
-	Fetch = mockFetch
+
+	// mock fetching contents
+	Fetch = func(request *Request) ([]byte, error) {
+		switch request.Path {
+		case "https://api.github.com/users/NDoolan360/repos?sort=stars":
+			return []byte(githubMock), nil
+		case "https://boardgamegeek.com/xmlapi/geeklist/332832":
+			return []byte(bggMock), nil
+		case "https://boardgamegeek.com/xmlapi/boardgame/330653":
+			return []byte(bggXMLMock), nil
+		case "https://cults3d.com/graphql":
+			return []byte(cults3DMock), nil
+		default:
+			return nil, errors.New("mock url not defined")
+		}
+	}
+
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
 			projects, errs := FetchProjects(tc.hosts)
-			if len(errs) > 0 {
+			if errs != nil {
 				t.Fatalf("Got error: %v", errs)
-			} else if !reflect.DeepEqual(projects, tc.want) {
-				t.Fatalf("Got %v;\nwant %v", projects, tc.want)
+			}
+			if len(tc.want) != len(projects) {
+				t.Fatalf("Expected projects to be len(%d), Got len(%d)", len(tc.want), len(projects))
+			}
+			for i, project := range projects {
+				if tc.want[i].Host != project.Host {
+					t.Fatalf("Host\nReceived: %v;\nExpected: %v", project.Host, tc.want[i].Host)
+				}
+				if tc.want[i].Title != project.Title {
+					t.Fatalf("Title\nReceived: %v;\nExpected: %v", project.Title, tc.want[i].Title)
+				}
+				if !reflect.DeepEqual(tc.want[i].Description, project.Description) {
+					t.Fatalf("Description\nReceived: %v;\nExpected: %v", project.Description, tc.want[i].Description)
+				}
+				if tc.want[i].Url != project.Url {
+					t.Fatalf("Url\nReceived: %v;\nExpected: %v", project.Url, tc.want[i].Url)
+				}
+				if !reflect.DeepEqual(tc.want[i].Image, project.Image) {
+					t.Fatalf("Image\nReceived: %v;\nExpected: %v", project.Image, tc.want[i].Image)
+				}
+				if !reflect.DeepEqual(tc.want[i].Language, project.Language) {
+					t.Fatalf("Image\nReceived: %v;\nExpected: %v", project.Language, tc.want[i].Image)
+				}
+				if !reflect.DeepEqual(tc.want[i].Logo, project.Logo) {
+					t.Fatalf("Logo\nReceived: %v;\nExpected: %v", project.Logo, tc.want[i].Logo)
+				}
+				if !reflect.DeepEqual(tc.want[i].Topics, project.Topics) {
+					t.Fatalf("Topics\nReceived: %v;\nExpected: %v", project.Topics, tc.want[i].Topics)
+				}
 			}
 		})
 	}
-}
-
-var mockFetch = func(url string) (string, error) {
-	switch url {
-	case "https://api.github.com/users/NDoolan360/repos?sort=stars":
-		return githubMock, nil
-	case "https://boardgamegeek.com/geeksearch.php?action=search&advsearch=1&objecttype=boardgame&include%5Bdesignerid%5D=133893":
-		return bggMock, nil
-	case "https://api.geekdo.com/xmlapi/boardgame/330653":
-		return bggXMLMock, nil
-	case "https://cults3d.com/en/users/ND360/3d-models":
-		return cults3DMock, nil
-	}
-	return "", errors.New("mock url not defined")
-}
-
-var GithubExpected = []Project{
-	{
-		Host:           "Github",
-		LogoSVG:        GetSVGLogo("github"),
-		Title:          "Test",
-		Description:    "My hand crafted Test",
-		HtmlUrl:        "https://github.com/NDoolan360/Test",
-		Topics:         []string{"test1", "test2", "test3"},
-		Fork:           false,
-		Language:       "Go",
-		LanguageColour: "#00ADD8",
-	},
-}
-
-var BggExpected = []Project{
-	{
-		Host:        "Board Game Geek",
-		LogoSVG:     GetSVGLogo("bgg"),
-		ImageSrc:    template.HTMLAttr(`src="https://cf.geekdo-images.com/wFwQ-MEGf6BLIyV77hQvHQ__original/img/jGDJHygR3da__4gT0pMzKAD1SQU=/0x0/filters:format(png)/pic5982841.png"`),
-		ImageAlt:    template.HTMLAttr(`alt="Board Game: Cake Toppers"`),
-		Title:       "Cake Toppers",
-		Description: "Bakers assemble the most outrageous cakes to top each other.",
-		HtmlUrl:     "https://boardgamegeek.com/boardgame/330653/cake-toppers",
-		Topics:      []string{"Hand Management", "Increase Value of Unchosen Resources", "Pattern Building", "Set Collection"},
-	},
-}
-
-var Cults3DExpected = []Project{
-	{
-		Host:     "Cults3D",
-		LogoSVG:  GetSVGLogo("cults3d"),
-		ImageSrc: template.HTMLAttr(`src="https://files.cults3d.com/uploaders/20027643/illustration-file/5371a13c-5cfa-4ce7-aebb-aedfa3865bd1/RRaPv2.png"`),
-		ImageAlt: template.HTMLAttr(`alt="RRaPv2.png Reciprocating Rack and Pinion Fidget V2"`),
-		Title:    "Reciprocating Rack and Pinion Fidget V2",
-		HtmlUrl:  "https://cults3d.com/en/3d-model/gadget/reciprocating-rack-and-pinion-fidget-v2",
-	},
-	{
-		Host:     "Cults3D",
-		LogoSVG:  GetSVGLogo("cults3d"),
-		ImageSrc: template.HTMLAttr(`src="https://files.cults3d.com/uploaders/20027643/illustration-file/9a4f2164-33a2-49ca-8b3b-2975c9bdf03b/RRaP2-Copy.png"`),
-		ImageAlt: template.HTMLAttr(`alt="RRaP2-Copy.png Thought Processor"`),
-		Title:    "Thought Processor",
-		HtmlUrl:  "https://cults3d.com/en/3d-model/art/thought-processor",
-	},
 }
 
 var githubMock = `
@@ -125,35 +108,14 @@ var githubMock = `
 ]`
 
 var bggMock = `
-<table>
-    <tbody>
-        <tr>
-            <th class="collection_thumbnail">
-                <span class="sr-only">Thumbnail image</span>
-            </th>
-            <th>
-                <a>Title</a>
-            </th>
-        </tr>
-        <tr id="row_">
-            <td class="collection_thumbnail">
-                <a href="/boardgame/330653/cake-toppers">
-                    <img alt="Board Game: Cake Toppers" src="https://cf.geekdo-images.com/wFwQ-MEGf6BLIyV77hQvHQ__micro/img/qOEv3ACF09F-_zGh0cSMIOXQrVs=/fit-in/64x64/filters:strip_icc()/pic5982841.png" />
-                </a>
-            </td>
-            <td>
-                <div>
-                    <a class="primary">Cake Toppers</a>
-                </div>
-                <p class="smallefont">Bakers assemble the most outrageous cakes to top each other.</p>
-            </td>
-        </tr>
-    </tbody>
-</table>`
+<geeklist>
+    <item objectid="330653" objectname="Cake Toppers"></item> 
+</geeklist>`
 
 var bggXMLMock = `
 <boardgames termsofuse="https://boardgamegeek.com/xmlapi/termsofuse">
     <boardgame objectid="330653">
+        <name primary="true" sortindex="1">Cake Toppers</name>
         <image>https://cf.geekdo-images.com/wFwQ-MEGf6BLIyV77hQvHQ__original/img/jGDJHygR3da__4gT0pMzKAD1SQU=/0x0/filters:format(png)/pic5982841.png</image>
         <boardgamemechanic objectid="2040">Hand Management</boardgamemechanic>
         <boardgamemechanic objectid="2914">Increase Value of Unchosen Resources</boardgamemechanic>
@@ -163,34 +125,90 @@ var bggXMLMock = `
 </boardgames>`
 
 var cults3DMock = `
-<article class="crea">
-  <div>
-    <a title="Reciprocating Rack and Pinion Fidget V2"
-      href="/en/3d-model/gadget/reciprocating-rack-and-pinion-fidget-v2">
-      <div>
-        <picture>
-          <img alt="RRaPv2.png Reciprocating Rack and Pinion Fidget V2"
-            data-src="https://images.cults3d.com/PFIDNlM1rYYHDszVySD-6bg0sJk=/246x246/filters:no_upscale()/https://files.cults3d.com/uploaders/20027643/illustration-file/5371a13c-5cfa-4ce7-aebb-aedfa3865bd1/RRaPv2.png">
-        </picture>
-      </div>
-      <div>
-        <h3>Reciprocating Rack and Pinion Fidget V2</h3>
-      </div>
-    </a>
-  </div>
-</article>
-<article class="crea">
-  <div>
-    <a title="Thought Processor" href="/en/3d-model/art/thought-processor">
-      <div>
-        <picture>
-          <img alt="RRaP2-Copy.png Thought Processor"
-            data-src="https://images.cults3d.com/BwnOBlJICQURW_aO68cA2AzELzA=/246x246/filters:no_upscale()/https://files.cults3d.com/uploaders/20027643/illustration-file/9a4f2164-33a2-49ca-8b3b-2975c9bdf03b/RRaP2-Copy.png">
-        </picture>
-      </div>
-      <div>
-        <h3>Thought Processor</h3>
-      </div>
-    </a>
-  </div>
-</article>`
+{
+  "data": {
+      "creationsBatch": {
+          "results": [
+              {
+                  "name": "Reciprocating Rack and Pinion Fidget V2",
+                  "url": "https://cults3d.com/en/3d-model/gadget/reciprocating-rack-and-pinion-fidget-v2",
+                  "illustrationImageUrl": "https://files.cults3d.com/uploaders/20027643/illustration-file/5371a13c-5cfa-4ce7-aebb-aedfa3865bd1/RRaPv2.png",
+                  "tags": [
+                      "rack",
+                      "pinion",
+                      "fidget",
+                      "toy",
+                      "reciprocating"
+                  ]
+              },
+              {
+                  "name": "Thought Processor",
+                  "url": "https://cults3d.com/en/3d-model/art/thought-processor",
+                  "illustrationImageUrl": "https://files.cults3d.com/uploaders/20027643/illustration-file/9a4f2164-33a2-49ca-8b3b-2975c9bdf03b/RRaP2-Copy.png",
+                  "tags": [
+                      "bust",
+                      "crt",
+                      "computer",
+                      "monitor",
+                      "display",
+                      "screen"
+                  ]
+              }
+          ]
+      }
+  }
+}`
+
+var GithubExpected = []Project{
+	{
+		Host:        "Github",
+		Logo:        GetSVGLogo("github"),
+		Title:       "Test",
+		Description: "My hand crafted Test",
+		Url:         "https://github.com/NDoolan360/Test",
+		Language: Language{
+			Name:   "Go",
+			Colour: template.CSS("#00ADD8"),
+		},
+		Topics: []string{"test1", "test2", "test3"},
+	},
+}
+
+var BggExpected = []Project{
+	{
+		Host:        "Board Game Geek",
+		Title:       "Cake Toppers",
+		Url:         "https://boardgamegeek.com/boardgame/330653",
+		Image: Image{
+			Src: template.HTMLAttr(`src="https://cf.geekdo-images.com/wFwQ-MEGf6BLIyV77hQvHQ__original/img/jGDJHygR3da__4gT0pMzKAD1SQU=/0x0/filters:format(png)/pic5982841.png"`),
+			Alt: template.HTMLAttr(`alt="Board Game: Cake Toppers"`),
+		},
+		Logo:   GetSVGLogo("bgg"),
+		Topics: []string{"Hand Management", "Increase Value of Unchosen Resources", "Pattern Building", "Set Collection"},
+	},
+}
+
+var Cults3DExpected = []Project{
+	{
+		Host:  "Cults3D",
+		Title: "Reciprocating Rack and Pinion Fidget V2",
+		Url:   "https://cults3d.com/en/3d-model/gadget/reciprocating-rack-and-pinion-fidget-v2",
+		Image: Image{
+			Src: template.HTMLAttr(`src="https://files.cults3d.com/uploaders/20027643/illustration-file/5371a13c-5cfa-4ce7-aebb-aedfa3865bd1/RRaPv2.png"`),
+			Alt: template.HTMLAttr(`alt="3D Model: Reciprocating Rack and Pinion Fidget V2"`),
+		},
+		Logo:   GetSVGLogo("cults3d"),
+		Topics: []string{"rack", "pinion", "fidget", "toy", "reciprocating"},
+	},
+	{
+		Host:  "Cults3D",
+		Title: "Thought Processor",
+		Url:   "https://cults3d.com/en/3d-model/art/thought-processor",
+		Logo:  GetSVGLogo("cults3d"),
+		Image: Image{
+			Src: template.HTMLAttr(`src="https://files.cults3d.com/uploaders/20027643/illustration-file/9a4f2164-33a2-49ca-8b3b-2975c9bdf03b/RRaP2-Copy.png"`),
+			Alt: template.HTMLAttr(`alt="3D Model: Thought Processor"`),
+		},
+		Topics: []string{"bust", "crt", "computer", "monitor", "display", "screen"},
+	},
+}
